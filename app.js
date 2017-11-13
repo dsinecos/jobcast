a031a9218d4ff9f6bbf32c1849f04dba2a43872b
Divyanshu Singh <dsinecos@gmail.com>
2017-11-13 13:55:03 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+module.exports = app;
