631878b936b8a1b3799fc49dcffca391239b71e5
Divyanshu Singh <dsinecos@gmail.com>
2017-11-14 15:21:52 +0530

    Use JSON web token for authentication and authorization

--- db/dbSetup.js
+++ db/dbSetup.js
@@ -1,19 +1,41 @@
 var jobcastDatabaseClient = require('./index.js');
 
 var queryToDropJobProfileTable = "DROP TABLE IF EXISTS jobcast_jobprofile";
+var queryToDropUserTable = "DROP TABLE IF EXISTS jobcast_users";
 var queryToGenerateJobProfileTable = "CREATE TABLE IF NOT EXISTS jobcast_jobprofile(jobprofile_id SERIAL PRIMARY KEY, companyname varchar(256) NOT NULL, companydescription varchar(256) NOT NULL, jobtitle varchar(256) NOT NULL, jobdescription varchar(256) NOT NULL)";
+var queryToGenerateUserTable = "CREATE TABLE IF NOT EXISTS jobcast_users(user_id SERIAL PRIMARY KEY, username varchar(256) NOT NULL, password varchar(256) NOT NULL, isadmin BOOLEAN NOT NULL)";
 
 jobcastDatabaseClient.query(queryToDropJobProfileTable)
     .then(function (data) {
 
-        jobcastDatabaseClient.query(queryToGenerateJobProfileTable)
+        jobcastDatabaseClient.query(queryToDropUserTable)
             .then(function (data) {
-                console.log("Job Profile table generated");
+
+                jobcastDatabaseClient.query(queryToGenerateJobProfileTable)
+                    .then(function (data) {
+                        console.log("Job Profile table generated");
+                    })
+                    .catch(function (error) {
+                        console.log("Error generating job profile table");
+                        console.log(error);
+                    })
+
+                jobcastDatabaseClient.query(queryToGenerateUserTable)
+                    .then(function (data) {
+                        console.log("User table generated");
+                    })
+                    .catch(function (error) {
+                        console.log("Error generating user table");
+                        console.log(error);
+                    })
+
             })
             .catch(function (error) {
-                console.log("Error generating job profile table");
+                console.log("Eror dropping user table");
                 console.log(error);
             })
+
+
 
     })
     .catch(function (error) {
